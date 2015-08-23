ac4ec115aa9427c8e3b260717a8b3d9c35089d74
Lee <lee-macdonald@hotmail.com>
2015-08-22 20:24:31 -0400

    pretty much done

--- home.php
+++ home.php
@@ -29,36 +29,60 @@
 			<div class="about-content-wrapper">
 				<div class="about-content">
 					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/PAM_2534.jpg">
-					<p>toronto based. business guy. web development guy. sports guy.</p>
+					<p>toronto based. business guy. web dev guy. sports guy.</p>
 				</div>
 			</div>
 		</section>
 
 		<section class="portfolio-section" id="portfolio">
-			<h2>Check Out My Work!</h2>
 			<div class="portfolio-content-wrapper">
-				
+				<h2>Check Out My Work!</h2>
+				<div class="project">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/road.jpeg" class="pic-one">
+					<div class="project-info one">
+						<h4>PSD Project</h4>
+						<p>This is that project where we did stuff, and then more stuff and
+						presented things and stuff. Cool story, Lee.</p>
+					</div>
+				</div>
+				<div class="project">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/road.jpeg" class="pic-two">
+					<div class="project-info two">
+						<h4>PSD Project</h4>
+						<p>This is that project where we did stuff, and then more stuff and
+						presented things and stuff. Cool story, Lee.</p>
+					</div>
+				</div>
+				<div class="project">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/road.jpeg" class="pic-three">
+					<div class="project-info three">
+						<h4>PSD Project</h4>
+						<p>This is that project where we did stuff, and then more stuff and
+						presented things and stuff. Cool story, Lee.</p>
+					</div>
+				</div>
 			</div>
 		</section>
 
 		<section class="contact-section" id="contact">
-			<h2>Get At Me!</h2>
 			<div class="contact-content-wrapper">
-				
-			</div>
-			<div class="social-icons">
-				<a href="mailto:lee@leemacd.com" target="_blank">
-					<i class="fa fa-envelope-o"></i>
-				</a>
-				<a href="http://twitter.com/leemacd" target="_blank">
-					<i class="fa fa-twitter"></i>
-				</a>
-				<a href="http://github.com/leemacd" target="_blank">
-					<i class="fa fa-github-alt"></i>
-				</a>
-				<a href="https://ca.linkedin.com/in/macdonaldlee" target="_blank">
-					<i class="fa fa-linkedin"></i>
-				</a>
+				<h2>Get At Me!</h2>
+				<div class="contact-content">
+					<a href="mailto:leemacd.com">
+						<p>lee@leemacd.com</p>
+					</a>
+					<div class="social-icons">
+						<a href="http://twitter.com/leemacd" target="_blank">
+							<i class="fa fa-twitter"></i>
+						</a>
+						<a href="http://github.com/leemacd" target="_blank">
+							<i class="fa fa-github-alt"></i>
+						</a>
+						<a href="https://ca.linkedin.com/in/macdonaldlee" target="_blank">
+							<i class="fa fa-linkedin"></i>
+						</a>
+					</div>
+				</div>
 			</div>
 		</section>
 
